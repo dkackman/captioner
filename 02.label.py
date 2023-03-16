4555ce1e85034f37712c1423a687782277eca7a7
Don Kackman <dkackman@gmail.com>
2023-03-16 15:21:57 -0500

    add readme

--- 02.label.py
+++ 02.label.py
@@ -7,13 +7,28 @@ from transformers import AutoProcessor, Blip2ForConditionalGeneration
 source_dir = '/mnt/data/photos/prepared/'
 csv_file_path = '/mnt/data/photos/labels.csv'
 
-model_name = "Salesforce/blip2-flan-t5-xxl"
+# need 24 GB of GPU memory for this one
+xxl = "Salesforce/blip2-flan-t5-xxl"
+xl = "Salesforce/blip2-flan-t5-xl"
+
+# change this to swapr models
+# in gneral bigger is better, slower, and requires better gpus
+model_name = xl
+
+# this controls the number of images sent to the gpu at once
+batch_size = 32
 
 # Load the model and processor
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model = Blip2ForConditionalGeneration.from_pretrained(
-    model_name, device_map="auto", load_in_8bit=True)  # .to(device)
+if model_name == xxl:
+    # use 8 bit inference if the model is xxl
+    model = Blip2ForConditionalGeneration.from_pretrained(
+        model_name, device_map="auto", load_in_8bit=True)
+else:
+    model = Blip2ForConditionalGeneration.from_pretrained(
+        model_name, device_map="auto", torch_dtype=torch.float16).to(device)
+
 processor = AutoProcessor.from_pretrained(model_name)
 
 
@@ -32,7 +47,7 @@ def get_image_labels(file_paths):
 
 # Calculate the total number of files to be processed
 file_paths = [os.path.join(source_dir, f) for f in os.listdir(
-    source_dir) if f.lower().endswith('.jpg')]
+    source_dir) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.heic'))]
 total_files = len(file_paths)
 file_counter = 0
 processed_counter = 0
@@ -42,7 +57,6 @@ with open(csv_file_path, 'w', newline='') as csvfile:
     csv_writer = csv.writer(csvfile)
     csv_writer.writerow(['file_path', 'label_name'])
 
-    batch_size = 32
     for i in range(0, total_files, batch_size):
         batch_file_paths = file_paths[i:i + batch_size]
         label_names = get_image_labels(batch_file_paths)
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Introduction
+
+## Install
+
+Make sure you have left the venv.
+
+```bash
+sh install.sh
+. ./activate
+```
