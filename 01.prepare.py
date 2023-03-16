8887a3e73e7384b1d5aa970898bb4a73bf34e694
Don Kackman <dkackman@gmail.com>
2023-03-15 19:43:27 -0500

    multithreaded

--- 01.prepare.py
+++ 01.prepare.py
@@ -1,60 +1,61 @@
 import os
-import shutil
-from pathlib import Path
-from PIL import Image
 import uuid
-import sys
+import shutil
+from PIL import Image
+from concurrent.futures import ThreadPoolExecutor
+
+source_dir = '/mnt/data/photos/original/'
+destination_dir = '/mnt/data/photos/prepared/'
 
 
-def convert_to_jpeg(src, dest):
+def process_image(file_path):
     try:
-        img = Image.open(src)
-        img.save(dest, 'JPEG')
-        return True
-    except Exception:
-        return False
-
-
-def process_images(src_dir, dest_dir):
-    src_path = Path(src_dir)
-    dest_path = Path(dest_dir)
-    dest_path.mkdir(parents=True, exist_ok=True)
-
-    copied_count = 0
-    ignored_count = 0
-    corrupt_count = 0
-    file_count = 0
-
-    total_files = sum(len(files) for _, _, files in os.walk(src_path))
-
-    for root, dirs, files in os.walk(src_path):
-        for file in files:
-            src_file = Path(root) / file
-            if src_file.stat().st_size < 1024 * 400:  # Ignore files smaller than 400 KB
-                ignored_count += 1
-                continue
-
-            dest_file = dest_path / f"{uuid.uuid4()}.jpg"
-
-            if convert_to_jpeg(src_file, dest_file):
-                copied_count += 1
+        with Image.open(file_path) as img:
+            # Check file size (width * height * 3 for RGB) and ignore files smaller than 400 KB
+            if img.size[0] * img.size[1] * 3 >= 400 * 1024:
+                img = img.convert('RGB')
+                destination_file = os.path.join(
+                    destination_dir, f"{uuid.uuid4()}.jpg")
+                img.save(destination_file)
+                return 'processed'
             else:
-                corrupt_count += 1
-
-            file_count += 1
-            sys.stdout.write(
-                f"\rProcessing file {file_count} of {total_files}")
-            sys.stdout.flush()
-
-    return copied_count, ignored_count, corrupt_count
+                return 'ignored'
+    except Exception:
+        return 'corrupt'
 
 
-if __name__ == "__main__":
-    source_dir = "/mnt/data/photos/original"
-    destination_dir = "/mnt/data/photos/prepared"
+# Create the destination directory if it does not exist
+os.makedirs(destination_dir, exist_ok=True)
 
-    copied, ignored, corrupt = process_images(source_dir, destination_dir)
+# Calculate the total number of files to be processed
+file_paths = []
+for root, _, files in os.walk(source_dir):
+    for file in files:
+        if file.lower().endswith(('.jpg', '.jpeg', '.png', '.heic')):
+            file_path = os.path.join(root, file)
+            file_paths.append(file_path)
 
-    print(f"\nNumber of files copied: {copied}")
-    print(f"Number of files ignored (size < 400 KB): {ignored}")
-    print(f"Number of corrupt files: {corrupt}")
+total_files = len(file_paths)
+file_counter = 0
+processed_counter = 0
+ignored_counter = 0
+corrupt_counter = 0
+
+# Process the files using ThreadPoolExecutor
+with ThreadPoolExecutor() as executor:
+    for file_path in file_paths:
+        file_counter += 1
+        result = executor.submit(process_image, file_path).result()
+
+        if result == 'processed':
+            processed_counter += 1
+        elif result == 'ignored':
+            ignored_counter += 1
+        elif result == 'corrupt':
+            corrupt_counter += 1
+        print(
+            f"Processed {processed_counter} of {total_files} files", end='\r')
+
+print(f"\nTotal files processed: {processed_counter}")
+print(f"Total files ignored: {ignored_counter}")
+print(f"Total files corrupt: {corrupt_counter}")
--- 02.preprocess.py
+++ 02.preprocess.py
@@ -3,7 +3,7 @@ import uuid
 from PIL import Image
 import torchvision.transforms as transforms
 import torch
-import sys
+from concurrent.futures import ThreadPoolExecutor
 
 # Define the image transformation pipeline
 transform = transforms.Compose([
@@ -16,30 +16,42 @@ transform = transforms.Compose([
 
 source_dir = '/mnt/data/photos/prepared/'
 destination_dir = '/mnt/data/photos/preprocessed/'
-file_counter = 0
-processed_counter = 0
+
+
+def preprocess_image(file_path):
+    try:
+        image = Image.open(file_path).convert('RGB')
+        input_tensor = transform(image)  # Transform the image to a tensor
+        destination_file = os.path.join(destination_dir, f"{uuid.uuid4()}.pt")
+        torch.save(input_tensor, destination_file)  # Save the tensor to disk
+        return 'processed'
+    except Exception:
+        return 'corrupt'
+
 
 # Create the destination directory if it does not exist
 os.makedirs(destination_dir, exist_ok=True)
 
-for root, _, files in os.walk(source_dir):
-    for file in files:
-        if file.lower().endswith(('.jpg', '.jpeg', '.png')):
-            file_counter += 1
-            file_path = os.path.join(root, file)
-            try:
-                image = Image.open(file_path).convert('RGB')
-                # Transform the image to a tensor
-                input_tensor = transform(image)
-                destination_file = os.path.join(
-                    destination_dir, f"{uuid.uuid4()}.pt")
-                # Save the tensor to disk
-                torch.save(input_tensor, destination_file)
-                processed_counter += 1
-                sys.stdout.write(
-                    f"\rProcessing file {processed_counter} of {file_counter}")
-            except Exception as e:
-                pass
+# Calculate the total number of files to be processed
+file_paths = [os.path.join(source_dir, f) for f in os.listdir(
+    source_dir) if f.lower().endswith('.jpg')]
+total_files = len(file_paths)
+file_counter = 0
+processed_counter = 0
+corrupt_counter = 0
+
+# Process the files using ThreadPoolExecutor
+with ThreadPoolExecutor() as executor:
+    for file_path in file_paths:
+        file_counter += 1
+        result = executor.submit(preprocess_image, file_path).result()
+
+        if result == 'processed':
+            processed_counter += 1
+        elif result == 'corrupt':
+            corrupt_counter += 1
+        print(
+            f"Processed {processed_counter} of {total_files} files", end='\r')
 
 print(f"\nTotal files processed: {processed_counter}")
-print(f"Total files skipped: {file_counter - processed_counter}")
+print(f"Total files corrupt: {corrupt_counter}")
